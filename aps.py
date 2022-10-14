d36e7423f897ae9e1bbc9272f805457d8619f02a
Caio Salesi <42592106+CaioSalesi@users.noreply.github.com>
2022-10-14 17:41:45 -0300

    Add files via upload

---
+++ aps.py
@@ -0,0 +1,163 @@
+print('Bem vindo à pesquisa de Créditos de Carbono')
+z = ('---------------------------')
+print(z)
+y = ('')
+a = 1
+while True:  
+    b = 1
+    if a == 1 or a== 2 or a==3 or a==4:
+        print('Digite o número de uma das opções: ')
+        print('1 - Crédito de Carbono')
+        print('2 - Grupo')
+        print('3 - Cálculos')
+        print('4 - Fechar o Programa')
+    a = int(input())
+    print(z)
+    if a == 1:
+        while b == 1 or b == 2 or b == 3 or b == 4 or b == 5 or b == 6 or b == 7 or b == 8:
+            print('1 - O que são créditos de carbono ?')
+            print('2 - Como os créditos de carbono são gerados e comercializados ?')
+            print('3 - Qual a relação do Brasil com isso ?')
+            print('4 - Iluminação pública e emissão de CO2')
+            print('5 - Eletrodomésticos aumentam as emissões ?')
+            print('6 - Como ocorrem as emissões de GEE na geração de energia elétrica ?')
+            print('7 - Você sabe quanto os carros ou motos emitem de gases poluentes ?')
+            print('8 - Os carros que dirigimos no dia-a-dia, quanto poluem ?')
+            print('9 - Voltar no programa')
+            b = int(input())
+            print(z)
+            if b == 1:
+                print('O crédito de carbono é um conceito que surgiu no ano de 1997, dentro do acordo ambiental do Protocolo de Kyoto. O grande objetivo relacionado a esse conceito é reduzir a emissão dos gases do efeito estufa no planeta para combater as mudanças climáticas que geram grande preocupação mundial.')
+                print(y)
+                print('Basicamente, o crédito de carbono é caracterizado como uma moeda utilizada no mercado de carbono, onde um crédito equivale a uma tonelada de CO2 (dióxido de carbono) que deixou de ser produzido e liberado ao meio ambiente.')
+                print(y)
+                print('Além do mais, quando outros gases deixam de ser lançados no meio ambiente, são emitidos créditos com base em uma tabela de carbono equivalente.')
+                print(y)
+                print('No mercado de crédito de carbono, as empresas que têm a possibilidade de diminuir a emissão de gases poluentes obtêm tais créditos. Estes, por sua vez, podem ser vendidos nos mercados financeiros nacionais e internacionais.')
+                print(y)
+                print('Já as empresas que possuem um nível de emissão de gases muito elevados, podem comprar créditos de carbono para compensar as emissões, investindo em processos sustentáveis de outras empresas.')
+                print(y)
+                print('Com isso, mesmo que indiretamente, 2 aspectos principais tornam-se possíveis: ')
+                print(y)
+                print('- Manutenção do projeto de redução das empresas que conseguem promover a redução da emissão de gases, além de equilibrar o nível de emissões de gases na atmosfera;')
+                print(y)
+                print('- Contribuem para o desenvolvimento sustentável de comunidades mais pobres.')
+                print(z)
+            if b == 2:
+                print('Os créditos são gerados a partir do investimento em ações que promovam estratégias que gerem economia de baixo carbono, como o uso de energias renováveis, caso da energia eólica, solar e as diversas formas de biomassa.')
+                print(y)
+                print('Dessa forma, as empresas que mais conseguem reduzir suas emissões de gases, geram mais créditos de carbono. Esses créditos, por sua vez, serão vendidos para empresas que não conseguem atingir os limites mínimos de redução, compensando suas emissões. Com isso, há benefícios para ambos os lados.')
+                print(y)
+                print('É importante ressaltar que as principais particularidades sobre a negociação dos créditos dependem da regulamentação de cada país. No Brasil, o mercado de créditos de carbono é regulamentado por meio do Decreto nº 5.882 de 2006.')
+                print(y)
+                print('A comercialização é feita conforme as regras do Mecanismo de Desenvolvimento Limpo (MDL), podendo ser unilateral, bilateral ou multilateral.')
+                print(y)
+                print('- Unilateral: essa modalidade relaciona-se aos projetos em desenvolvimento que ocorrem no próprio território. Esses projetos devem promover a redução da emissão de CO2, gerando créditos que poderão ser comercializados a um valor definido pelo próprio país que desenvolveu o projeto em território nacional.')
+                print(y)
+                print('- Bilateral: nessa modalidade, os projetos elaborados por um país em desenvolvimento podem ser executados em um segundo país em desenvolvimento, que será denominado como “país hospedeiro”. Nela, o CO2 não emitido à atmosfera gerará créditos para o país que elaborou e implementou os projetos.')
+                print(y)
+                print('- Multilateral: na terceira modalidade os projetos e as estratégias de redução de emissão de CO2 são implementados e financiados por fundos internacionais. Nessa modalidade, o valor do crédito de carbono é definido por esses fundos de investimento.')
+                print(z)
+            if b == 3:
+                print('Pelas suas características baseadas em sustentabilidade, o Brasil possui grande representatividade no mercado de carbono, ficando atrás somente da Índia e da China.')
+                print(y)
+                print('Uma grande parte destes projetos tem relação direta com a biomassa, visto que a indústria brasileira usufrui de forma significativa no uso dessa fonte energética, considerada limpa e ecologicamente correta.')
+                print(y)
+                print('Diante disso, o Brasil é um dos países que apresentam estágios mais avançados em termos de preparação para o Protocolo de Kyoto e consequente produção de créditos de carbono, devido à ampla participação governamental e das empresas de ramos variados.')
+                print(y)
+                print('Assim, as perspectivas de crescimento do mercado de carbono são bastante otimistas para empresas brasileiras, inclusive, com possibilidades de rendimentos extras para indústrias dos mais variados setores e ramos de atuação.')
+                print(y)
+                print('Ou seja, o Brasil pode ser um dos grandes beneficiados no promissor mercado do carbono, sendo um importante gerador e vendedor de créditos de carbono.')
+                print(y)
+                print('Estimativas recentes indicam que o potencial de geração de receitas com créditos de carbono até 2030 para o Brasil gira em torno de US$493 milhões e US$100 bilhões. Isso equivaleria a 1 gigaton (1 bilhão de toneladas de CO2 equivalentes) ao longo da próxima década para os setores de agro, floresta e energia.')
+                print(z)
+            if b == 4:
+                print('A iluminação pública é responsável por quase 25% da emissão de CO2 no mundo, um gás causador do efeito estufa.')
+                print(y)
+                print('Por isso, ela se tornou parte das pautas globais sobre os impactos da emissão de C02 para o planeta. Para você entender melhor, saiba que apenas uma unidade de lâmpada incandescente solta 2,7 kg de CO2 na atmosfera.')
+                print(y)
+                print('Isso fez com que a sua participação no cenário da iluminação começasse a ser questionada. O fato é que os efeitos do aquecimento global são cada vez mais perceptíveis. Desse modo, toda tecnologia que represente uma ameaça deve ser trocada por outra mais sustentável.')
+                print(z)
+            if b == 5:
+                print('Não, nem todos os eletrodomésticos emitem gases de efeito estufa diretamente, como se verá mais adiante. Mas todos consomem muita energia elétrica. E o setor de energia emite gases. Assim, seus eletrodomésticos colaboram indiretamente nessas emissões. Quanto mais energia elétrica eles consomem, mais gases o setor emite. As residências brasileiras foram responsáveis por 29% de toda a energia consumida no País, em 2018.')
+                print(z)
+            if b == 6:
+                print('A queima de combustíveis fósseis para a geração de energia elétrica é o principal contribuinte para a emissão de GEE poluentes como dióxido de carbono (CO2), metano (CH4) e óxido nitroso (N2O), entre outros, impactando a saúde pública e agravando a crise climática. Isso quer dizer que além de utilizar recursos naturais finitos, a energia produzida por meio da queima do petróleo, do carvão mineral ou do gás natural (termelétrica) e da fissão nuclear são menos sustentáveis por emitir grande quantidade de GEE.')
+                print(z)
+            if b == 7:
+                print('Primeiramente, precisamos saber como quantificar o volume de emissões que os veículos automotores geram.')
+                print(y)
+                print('Em uma combustão ideal, todo o combustível queimado deveria virar gás carbônico (CO2). O gás carbônico não é considerado um poluente por não prejudicar o organismo humano, mas é um gás estufa. Daí a preocupação com a quantidade de sua emissão, pois seu volume gerado se mostra muito grande em relação aos outros poluentes.')
+                print(y)
+                print('Assim, os engenheiros de motores convencionaram medir o nível de emissão de poluentes de um veículo em gramas de gás carbônico por quilômetro rodado, cuja notação é gCO2/km.')
+                print(y)
+                print('No mundo real, a combustão nunca é completa, e outros poluentes muito mais nocivos advêm do funcionamento dos motores, como o monóxido de carbono (CO), hidrocarbonetos (HC) e óxido nitroso (NOx). Saiba mais sobre os gases poluentes neste artigo:')
+                print(y)
+                print('Conheça os gases poluentes que seu carro emite: ')
+                print(y)
+                print('Os gases acima, produtos de uma combustão incompleta, causam sérios danos à saúde e ao meio ambiente, por isso as entidades homologadoras e os fabricantes consideram que sua emissão deve ser zero. A forma mais eficiente de reduzir a emissão desses resíduos consiste no aprimoramento da tecnologia de catalisadores, pois a mera redução de consumo de combustível não implica necessariamente sua mitigação.')
+                print(y)
+                print('Por sua vez, o gás carbônico se mostra um produto natural do funcionamento dos motores, mas busca-se sempre melhorar a eficiência energética. Para avaliar se o resultado está sendo alcançado, os engenheiros devem perseguir a redução das emissões de CO2, um indício de redução de consumo de combustível. Ele se mostra o principal indicador de melhoria de aproveitamento do poder calorífico do combustível.')
+                print(y)
+                print('Com o passar do tempo e o avanço da tecnologia de motores, o consumo de combustível se reduz paulatinamente, ao passo que o desempenho melhora. A partir dessa constatação de fácil percepção, podemos observar a melhoria na quantidade de gás carbônico emitido. As linhas abaixo trazem uma estimativa de gramas por quilômetro rodado ao longo do tempo, junto com as inovações tecnológicas responsáveis por sua queda:')
+                print(y)
+                print('Nota: os números inferiores descrevem os modelos menos poluentes da época, como os modelos populares e/ou híbridos e elétricos; os superiores relatam os modelos mais poluentes, os quais costumam ser os de alta potência, com motores grandes potentes e beberrões, como os V8 americanos e superesportivos como Ferrari e Lamborghini.')
+                print(y)
+                print('- Anos 1940 – acima de 5000 gCO2/km. A partir desta época, novas tecnologias foram implementadas para redução de consumo de combustível, consequentemente impactando nas emissões.')
+                print(y)
+                print('- Anos 1970 – 1500 a 4000 gCO2/km. A crise do petróleo obrigou os fabricantes a implantar uma nova onda de novas soluções para reduzir o consumo.')
+                print(y)
+                print('- Anos 1980 – 500 a 2000 gCO2/km. Início da aplicação de catalisadores em larga escala. Legislações ambientais mais rígidas e necessidade de testes de homologação trouxeram rápida redução das emissões.')
+                print(y)
+                print('- Anos 1990 – 300 a 1000 gCO2/km. O aumento das restrições legais universalizou o uso dos catalisadores e surgimento de novas tecnologias como injeção eletrônica e cabeçote multiválvulas se disseminou.')
+                print(y)
+                print('- Anos 2000 – 100 a 500 gCO2/km. A disseminação do downsizing de motores e o surgimento da motorização híbrida e elétrica contribuiu para a rápida queda nas emissões.')
+                print(y)
+                print('- 2010 até o momento – zero a 400 gCO2/km. Veículos de emissão zero já são vendidos em escala comercial e os modelos a combustão se encontram em estágio tecnológico avançado na maior parte do mundo. A média de emissões gira entre 130 a 250 gCO2/km nos dias de hoje.')
+                print(y)
+                print('- Projeção para o futuro – Com a renovação da frota e contínua redução de emissões, países como a Alemanha desejam produzir e comercializar apenas modelos que não poluem até 2030 e eliminar os veículos a combustão das ruas até 2050, exceto os de coleção. O resto do mundo deve seguir seus passos.')
+                print(z)
+            if b == 8:
+                print('Os modelos menos poluentes são os híbridos comercializados atualmente no Brasil (2017), notadamente o Toyota Prius, Lexus CT200h e Ford Fusion. Todos emitem entre 50 e 80 gCO2/km. O grupo PSA fez um ótimo trabalho no seu motor 1.2 Puretech, com todos os modelos emitindo abaixo de 100 gCO2/km. A Volkswagen também possui modelos que emitem menos de 100 gCO2/km em 2017.')
+                print(y)
+                print('Na média, os modelos zero quilômetro emitem entre 100 e 300 gramas de gás carbônico por quilômetro, entre compactos de entrada e SUV´s grandes a diesel.')
+                print(y)
+                print('Confira o quanto seu carro emite neste documento de homologação da CONPET, entidade responsável pela homologação de eficiência energética.')
+                print(y)
+    elif a == 2:
+        print('Caio Vinícius Salesi')
+        print('Henrique... ')
+        print('José João Henrique Trindade Oliveira')
+        print('Thaissa... ')
+        print(z)
+    elif a == 3:
+        b = 3
+        while b != 1 and b != 2:
+            print('Qual o tipo de combustível de seu carro ?')
+            print('1 - Gasolina')
+            print('2 - Diesel')
+            b = int(input())
+            print('Digite quantos km você pretende andar')
+            km = float(input())
+            if b == 1:
+                l = km*10
+                c = l*0.82*0.75*3.7
+                cc = c/1000*365
+                print(f'Você gastará {l}L para essa viagem e gerará {round(c,2)}kg de CO2, que em crédito carbono, vale R${round(cc, 2)}')
+                print(z)
+            elif b == 2:
+                l = km*3
+                c = l*0.83*3.7
+                cc = c/1000*365
+                print(f'Você gastará {l}L para essa viagem e gerará {round(c,2)}kg de CO2, que em crédito carbono, vale R${round(cc, 2)}')
+                print(z)
+            else:
+                print('Digite um valor valido')
+    elif a == 4:
+        break
+    else:
+        print('Digite um valor valido: ')
+print('Obrigado por usar nosso programa')
+
+
+        
